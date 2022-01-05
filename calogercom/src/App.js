4de964c5a99237d2e8fc2a6f31e64dbc0f943f71
Matthew Caloger <mattcaloger@gmail.com>
2022-01-04 23:50:06 -0500

    Terminal addition

--- calogercom/src/App.js
+++ calogercom/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import Terminal from './Components/Terminal/Terminal';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Terminal />
     </div>
   );
 }
---
+++ calogercom/src/Components/Terminal/Terminal.jsx
@@ -0,0 +1,25 @@
+
+
+        import React from 'react'
+        
+        export default function Terminal() {
+            return (
+                 
+ <div className="card">
+            <p>$ list skills</p>
+            <p><span className="color-js">js</span> <span className="color-html">html</span> <span className="color-css">css</span>
+                <span className="color-php">php</span> <span className="color-node">node</span> <span
+                    className="color-csharp">csharp</span> <span className="color-java">java</span> <span
+                    className="color-reactjs">reactjs</span> <span className="color-spring">spring</span> <span
+                    className="color-photoshop">photoshop</span> <span className="color-illustrator">illustrator</span> <span
+                    className="color-typescript">typescript</span> <span className="color-sql">sql</span>
+            </p>
+            <p>$ show interests</p>
+            <p>programming system-design project-management graphic-design </p>
+            <p>$ display location</p>
+            <p>Toronto</p>
+            <p>$ <span className="blink">_</span></p>
+        </div>
+            )
+        }
+        
