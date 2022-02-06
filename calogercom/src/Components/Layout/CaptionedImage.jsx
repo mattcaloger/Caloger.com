dec34df69f8828b5ab6721c0df891ec881f557c7
mcaloger <mattcaloger@gmail.com>
2022-02-06 11:29:44 -0500

    changing the look of images to remove shadow effect. The purpose is to make it more clear what is interactive and what is not.

--- calogercom/src/Components/Layout/CaptionedImage.jsx
+++ calogercom/src/Components/Layout/CaptionedImage.jsx
@@ -10,7 +10,7 @@ export const DisplayImage = styled.img`
     max-width: 100%;
     max-height: 100%;
     border-radius: 10px;
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+
 `
 
 export const ImageContainer = styled.div`
--- calogercom/src/Components/Pages/ProjectsPage/Projects/Layout/BlogImage.jsx
+++ calogercom/src/Components/Pages/ProjectsPage/Projects/Layout/BlogImage.jsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 const BlogImage = styled.img`
     width: 100%;
     height: auto;
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
     border-radius: 25px;
 `
 
